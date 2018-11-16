d1b7bde4840cea9b2cce0e8d8fd3f4a740ebbc0c
Sarah Zhou <jxsz@berkeley.edu>
2018-11-16 20:26:07 +0000

    add verfication to gapbs.sh and .json and run.sh

--- gapbs.sh
+++ gapbs.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
+#TODO: add function usage informations. add verify tag
+verify=0
+function usage
+{
+    echo "usage: gapbs.sh <workload-name> [-H | -h | --help] [--verify]"
+    echo "    workload-name: the kernel and graph input"
+    echo "    verify: if set, verifies the output of the benchmark. Default is off"
+}
+
+if [ $# -eq 0 -o "$1" == "--help" -o "$1" == "-h"]; then
+    usage
+    exit 3
+fi
+
 bmark_name=$1
+shift
+if [ "$1" == "--verify" ]; then
+    verify=1
+fi
+
 mkdir -p ~/output
 export OMP_NUM_THREADS=$2
 echo "Starting rate $bmark_name run with $OMP_NUM_THREADS threads"
--- gen_run_scripts.sh
+++ gen_run_scripts.sh
@@ -1,10 +1,44 @@
 #!/bin/bash
-command_file=bmark.cmd
+#default values
+command_file=test.cmd
 t_pwd="/gapbs"
+jsonFlag=false
+input_type=test
+verifyFlag=false
 
 workload_file="gapbs.json"
+function usage
+{
+    echo "usage gen_run_scripts.sh [--json] [-h] --input [test | graph500 | ref] [--verify]"
+}
+
+while test $# -gt 0
+do
+    case "$1" in
+         --json)
+             jsonFlag=true
+             ;;
+         --input)
+             shift;
+             input_type=$1
+             command_file=$1.cmd
+             ;;
+         -h)
+             usage
+             exit
+             ;;
+         --verify)
+             verifyFlag=true
+             ;;
+    esac
+    shift
+done
+
+
 mkdir -p run
-echo "[" > $workload_file
+if [ "$jsonFlag" = true ]; then
+    echo "{" > $workload_file
+fi
 
 while IFS= read -r command; do
     bmark=`echo $command | sed 's/\.\/\([a-z]*\).*/\1/'`
@@ -12,24 +46,31 @@ while IFS= read -r command; do
     output_file="${t_pwd}/`echo $command | grep -Eo "benchmark\/out/.*out"`"
     binary="${bmark}"
     workload=$(basename $output_file .out)
-    echo ${workload}
-    echo ${output_file}
-    echo ${graph}
-    echo ${binary}
-#    run_script=run/${workload}.sh
-#    echo '#!/bin/bash' > $run_script
-#    echo $command | sed "s/benchmark/\\${t_pwd}\/benchmark/g" | sed "s/^\./\\${t_pwd}/" >> $run_script
-#    chmod +x $run_script
+    echo "workload: ${workload}"
+    echo "output_file: ${output_file}"
+    echo "graph: ${graph}"
+    echo "binary: ${binary}"
+    run_script=run/${workload}.sh
+    echo '#!/bin/bash' > $run_script
+    if [ "$verifyFlag" = true ]; then
+        echo $command | sed "s/benchmark/\\${t_pwd}\/benchmark/g" | sed "s/^\./\\${t_pwd}/" | sed "s/-n/-vn/g" >> $run_script
+    else
+        echo $command | sed "s/benchmark/\\${t_pwd}\/benchmark/g" | sed "s/^\./\\${t_pwd}/" >> $run_script
+    fi
 
-    echo "  {" >> $workload_file
-    echo "    \"name\": \"${workload}\"," >> $workload_file
-    echo "    \"files\": [\"${binary}\", \"${graph}\"]," >> $workload_file
-    echo "    \"command\": \"cd /gapbs && ./gapbs.sh ${workload}\"," >> $workload_file
-    echo "    \"outputs\": []" >> $workload_file
-    echo "  }," >> $workload_file
-
+    chmod +x $run_script
+    cat $run_script
+    if [ "$jsonFlag" = true ]; then
+        echo "  {" >> $workload_file
+        echo "    \"name\": \"${workload}\"," >> $workload_file
+        echo "    \"files\": [\"${binary}\", \"${graph}\"]," >> $workload_file
+        echo "    \"command\": \"cd /gapbs && ./gapbs.sh ${workload}\"," >> $workload_file
+        echo "    \"outputs\": []" >> $workload_file
+        echo "  }," >> $workload_file
+    fi
 done < $command_file
-echo "$(head -n -1 $workload_file)" > $workload_file
-echo "  }" >> $workload_file
-echo "]" >> $workload_file
-
+if [ "$jsonFlag" = true ]; then
+    echo "$(head -n -1 $workload_file)" > $workload_file
+    echo "  }" >> $workload_file
+    echo "]" >> $workload_file
+fi
