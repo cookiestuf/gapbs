07c75643533810c7f82cb0a29abede542bd94be1
Sarah Zhou <jxsz@berkeley.edu>
2018-11-22 00:04:09 +0000

    remove hpm monitoring from gapbs.sh

--- gapbs.sh
+++ gapbs.sh
@@ -17,14 +17,9 @@ shift
 mkdir -p ~/output
 export OMP_NUM_THREADS=`grep -o 'hart' /proc/cpuinfo | wc -l`
 echo "Starting rate $bmark_name run with $OMP_NUM_THREADS threads"
-./start_counters
 if [ "$1" == "--verify" ]; then
     echo "and verifying output."
     ./run/${bmark_name}.sh -v > ~/output/out 2>~/output/err
 else
     ./run/${bmark_name}.sh > ~/output/out 2>~/output/err
 fi
-./stop_counters
-cd ~/hpm_data
-head -2 *0*
-tail -n2 *0*
